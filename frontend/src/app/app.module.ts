788e914c32cb0bc7330b071da54933ad13b012dc
Amar <amar.vilegave04@gmail.com>
2023-06-24 16:47:07 +0530

    Created new component and imported forms module to app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/userInterface/home/home.component';
 import { AboutPageComponent } from './Components/userInterface/about-page/about-page.component';
 import { ContactPageComponent } from './Components/userInterface/contact-page/contact-page.component';
+import { UserRegistrationPageComponent } from './Components/userInterface/user-registration-page/user-registration-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutPageComponent,
-    ContactPageComponent
+    ContactPageComponent,
+    UserRegistrationPageComponent
   ],
   imports: [
     BrowserModule,
