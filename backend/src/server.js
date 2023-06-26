af6afa9c5c6d5bbbb83f289fef85fa1665439498
Amar <amar.vilegave04@gmail.com>
2023-06-26 23:03:09 +0530

    changed location of server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const config = require("config");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/grocery-shopping")
