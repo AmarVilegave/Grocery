6a658d787bc5f8b6d4f329ffbf7401d2dfe025f6
Amar <amar.vilegave04@gmail.com>
2023-06-29 21:19:58 +0530

    for user/admin login/logout, registration functionality added, new classes & interfaces added

---
+++ frontend/src/app/shared/classes/UserModel.ts
@@ -0,0 +1,8 @@
+export class UserModel {
+  fullName: string = null;
+  email: string = null;
+  password: string = null;
+  mobileNo: number = null;
+  dateOfBirth: string = null;
+  isAdmin: boolean = false;
+}
---
+++ frontend/src/app/shared/interfaces/databaseAddress.ts
@@ -0,0 +1,7 @@
+export interface databaseAddress {
+  address: string;
+  landmark: string;
+  state: string;
+  city: string;
+  pincode: number;
+}
